78f40192a3da9da92bab6e9e8e229a404f423fa9
Will Vetter <vetter.william@gmail.com>
2020-08-01 18:47:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,3 +2,10 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if [" ", "", nil].include? board[index]
+    return false
+  else
+    return true
+  end
+end
